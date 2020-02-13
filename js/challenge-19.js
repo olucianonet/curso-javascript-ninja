7669b8ee72d2177cf4ab2618f38f28ea83b28de7
Luciano Cunha <oluciano.net@gmail.com>
2020-02-12 21:48:29 -0300

    19. Regex 3

--- js/challenge-19.js
+++ js/challenge-19.js
@@ -1,3 +1,5 @@
+(function(){
+    'use strict';
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
@@ -23,16 +25,21 @@ linha, independente de quantos caracteres de número estiverem juntos.
 - Atribua essa regex à uma variável chamada `justNumbersRegex` e mostre-a
 no console:
 */
+
 console.log( 'Regex para números usando o construtor:' );
-// ?
+var justNumbersRegex = new RegExp('^\\d+', 'gm');
 
 /*
 Verifique se a regex acima casa com o texto na variável `text`, mostrando o
 resultado no console. O resultado deve ser:
 "[ '10', '50' ]"
 */
-var text = '10 anos.\n50 discos vendidos.\nE nem 10% dos meus amigos o conhece.';
+
+var text = 
+    '10 anos.\n50 discos vendidos.\nE nem 10% dos meus amigos o conhece.';
 console.log( '\nNúmeros no início da linha do texto:\n' + text, '\n' );
+console.log(text.match(justNumbersRegex));
+
 // ?
 
 /*
@@ -43,8 +50,9 @@ que esta tenha muitas linhas, deve sempre casar com números no fim de cada
 linha, independente de quantos caracteres de número estiverem juntos.
 Mostre a regex no console:
 */
+
 console.log( '\nRegex para números somente no final das linhas:' );
-// ?
+var numbersAtTheEnd = new RegExp('\\d+$', 'gm');
 
 /*
 Verifique se a regex acima casa com o texto na variável `otherText`,
@@ -52,9 +60,11 @@ mostrando o resultado no console.
 O resultado deve ser:
 "[ '12', '6' ]"
 */
+
 var otherText = 'Silvio Santos, nome artístico de Senor Abravanel (Rio de Janeiro, 12\n de dezembro de 1930), é um apresentador de televisão e empresário brasileiro.\n Proprietário do Grupo Silvio Santos, que inclui empresas como a Liderança\n Capitalização (administradora da loteria Tele Sena), a Jequiti Cosméticos e o\n Sistema Brasileiro de Televisão (mais conhecido como SBT), Silvio Santos possui\n um patrimônio avaliado em aproximadamente 6\n bilhões de reais.';
 console.log( '\nNúmeros no final da linha:\n\n', otherText, '\n' );
-// ?
+console.log(otherText.match(numbersAtTheEnd));
+
 
 /*
 Vamos criar um método que vai testar se uma classe CSS existe em uma
@@ -79,6 +89,22 @@ abaixo;
 qualquer classe que for testada. Os dados passados no exercício são somente
 para exemplificar.
 */
+
+function hasClass(markup, cssClass){ 
+    var regx = new RegExp(
+    `(class=["'](${cssClass})?(\s?)(${cssClass})?["']?)|(?:\D)${cssClass}(\D)`,
+    'gm');
+    console.log(regx);
+    return regx.test(markup);
+}
+
 var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
-console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
-// ?
+console.log(
+    '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
+
+var cssClasses = ["container", "text", "date", "excerpt", "main", "luciano"];
+cssClasses.forEach(function(element){
+    console.log(`${hasClass(markup, element)} para a classe "${element}"`);
+    });
+
+})();
