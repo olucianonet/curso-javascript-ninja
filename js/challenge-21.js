d8bd46ce6dcef895f33bbad334718b8de6f5fd16
Luciano Cunha <oluciano.net@gmail.com>
2020-02-15 13:16:31 -0300

    21. Sync versus Async

--- js/challenge-21.js
+++ js/challenge-21.js
@@ -1,3 +1,6 @@
+(function(window, document) {
+    'use strict';
+
 /*
 O desafio de hoje será um pequeno projeto: um cronômetro!
 As regras para criação do cronômetro são as seguintes:
@@ -14,4 +17,29 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-// ?
+
+    var $textDisplay = document.querySelector('[data-js="display"]');
+    var $buttonStart = document.querySelector('[data-js="start"]');
+    var $buttonStop = document.querySelector('[data-js="stop"]');
+    var $buttonReset = document.querySelector('[data-js="reset"]');
+
+    var myTimer;    
+
+    $buttonStart.addEventListener('click', start, false);
+    $buttonStop.addEventListener('click', stop, false);
+    $buttonReset.addEventListener('click', reset, false);
+
+    function start(){
+        $textDisplay.value = +$textDisplay.value + 1;
+        myTimer = setTimeout(start, 1000);
+    }
+
+    function stop(event){
+        clearTimeout(myTimer);
+    }
+
+    function reset(event){
+        $textDisplay.value = 0;
+    }
+
+})(window, document);
